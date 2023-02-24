28f123c0659986363df81ec21bd1ba62c4cc9c15
Lothar Schulz <lothar@miro.com>
2023-02-24 16:50:47 +0100

    chore deal with redis not being available

--- app/src/main/kotlin/redis_kotlin/App.kt
+++ app/src/main/kotlin/redis_kotlin/App.kt
@@ -2,15 +2,20 @@ package redis_kotlin
 
 import org.redisson.Redisson
 import org.redisson.api.RedissonClient
+import org.redisson.client.RedisConnectionException
 import org.redisson.config.Config
 
 
 class App {
-    private fun redissonClient(): RedissonClient {
+    private fun redissonClient(): RedissonClient? { // TODO no nullable return value as in https://www.lotharschulz.info/2022/07/28/replace-null-with-amazing-kotlin-and-java-sealed-classes-interfaces/
         // connects to 127.0.0.1:6379 by default
         val config = Config()
         config.useSingleServer().address = "redis://localhost:6379" // in case you need to set it
-        return Redisson.create(config)
+        return try {
+            Redisson.create(config)
+        } catch (rce: RedisConnectionException) {
+            null
+        }
     }
 
     private fun bucketSetGet(redissonClient: RedissonClient, bucketName: String, value: String){
@@ -33,10 +38,16 @@ class App {
         // Services
         // Pipelining
         // Scripting
-        bucketSetGet(redisson, "foo", "bar") // buckets
-        // close the client
-        redisson.shutdown()
-        return true;
+        return try {
+            val redissonNotNull = checkNotNull(redisson) { "State must be set beforehand" }
+            bucketSetGet(redissonNotNull, "foo", "bar") // buckets
+            // close the client
+            redissonNotNull.shutdown()
+            true;
+        } catch (e: Exception) {
+            println("can not connect to redis, please check if redis container is running")
+            false
+        }
     }
 
 }
