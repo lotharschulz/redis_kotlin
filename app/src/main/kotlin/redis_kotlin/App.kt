8297691b878dc69528d8281b5c07c30b98664764
Lothar Schulz <lothar.sch@gmx.de>
2023-03-01 19:56:55 +0100

    chore atomic long async code reformatting

--- app/src/main/kotlin/redis_kotlin/App.kt
+++ app/src/main/kotlin/redis_kotlin/App.kt
@@ -1,13 +1,11 @@
 package redis_kotlin
 
 import org.redisson.Redisson
-import org.redisson.api.RBucket
-import org.redisson.api.RKeys
-import org.redisson.api.RedissonClient
+import org.redisson.api.*
 import org.redisson.client.RedisConnectionException
 import org.redisson.config.Config
 import java.io.Serializable
-import javax.xml.crypto.dsig.keyinfo.KeyName
+
 
 data class Book(val pages: Int, val chapter: Int, val author: String) : Serializable
 class App {
@@ -22,25 +20,91 @@ class App {
         }
     }
 
-    private fun keysSetGet(redissonClient: RedissonClient, keyName1: String, keyName2: String) {
-        printHelper("keysSetGet")
-        val rKeys: RKeys = redissonClient.keys
-        redissonClient.getBucket<Int>(keyName1).set(1)
-        redissonClient.getBucket<Int>(keyName2).set(1)
-        val keys = rKeys.keys
-        println("default sorted (almost lexicographically) keys:")
-        keys.sorted().forEach { println(it) }
+    private fun atomicLong(redisson: RedissonClient, newValue: Long) {
+        printHelper("atomicLong")
+        val myAtomicLong: RAtomicLong = redisson.getAtomicLong("myAtomicLong")
+        println("initial myAtomicLong: $myAtomicLong")
+        myAtomicLong.set(newValue)
+        println("myAtomicLong after set to $newValue: $myAtomicLong")
+        myAtomicLong.incrementAndGet()
+        println("myAtomicLong after increment and set: $myAtomicLong")
+        myAtomicLong.get()
+        println("myAtomicLong after get: $myAtomicLong")
+        myAtomicLong.unlink() // clean up, happens async
+        println("myAtomicLong after unlink/cleanup: $myAtomicLong")
     }
 
-    private fun bucketGetSet(redissonClient: RedissonClient, bucketName: String, value: String) {
-        printHelper("bucketGetSet")
+    private fun atomicLongAsync(redisson: RedissonClient, newValue: Long) {
+        printHelper("atomicLong async interface")
+        val myAtomicLong: RAtomicLong = redisson.getAtomicLong("myAtomicLong")
+        println("initial myAtomicLong: $myAtomicLong")
+        val setFuture: RFuture<Void> = myAtomicLong.setAsync(newValue)
+        try {
+            val setResult = setFuture.toCompletableFuture().get()
+            println("async setResult: $setResult")
+        } catch (e: Exception) {
+            println("after async set an exception happened: ${e.localizedMessage}")
+        }
+
+        val getFuture: RFuture<Long> = myAtomicLong.getAsync()
+        try {
+            val result = getFuture.toCompletableFuture().get()
+            println("myAtomicLong after async get result: $result")
+        } catch (e: Exception) {
+            println("after async get an exception happened: ${e.localizedMessage}")
+        }
+
+        val igFuture: RFuture<Long> = myAtomicLong.incrementAndGetAsync()
+        try {
+            val result = igFuture.toCompletableFuture().get()
+            println("myAtomicLong after async increment and set result: $result")
+        } catch (e: Exception) {
+            println("after async set an exception happened: ${e.localizedMessage}")
+        }
+
+        val getFuture2: RFuture<Long> = myAtomicLong.getAsync()
+        try {
+            val result = getFuture2.toCompletableFuture().get()
+            println("myAtomicLong after async get result: $result")
+        } catch (e: Exception) {
+            println("after async get an exception happened: ${e.localizedMessage}")
+        }
+
+        myAtomicLong.unlink() // clean up, happens async
+        println("myAtomicLong after unlink/cleanup: $myAtomicLong")
+    }
+
+    // todo: wrap into coroutines
+
+    // todo reactive interface
+    /*
+    RedissonReactiveClient redisson = redissonClient.reactive();
+    RAtomicLongReactive atomicLong = redisson.getAtomicLong("myAtomicLong");
+
+    Mono<Void> setMono = atomicLong.set(3);
+    Mono<Long> igMono = atomicLong.incrementAndGet();
+    RFuture<Long> getMono = atomicLong.getAsync();
+     */
+
+    // todo RxJava3
+    /*
+    RedissonRxClient redisson = redissonClient.rxJava();
+    RAtomicLongRx atomicLong = redisson.getAtomicLong("myAtomicLong");
+
+    Completable setMono = atomicLong.set(3);
+    Single<Long> igMono = atomicLong.incrementAndGet();
+    Single<Long> getMono = atomicLong.getAsync();
+     */
+
+    private fun bucket(redissonClient: RedissonClient, bucketName: String, value: String) {
+        printHelper("bucket")
         val bucket = redissonClient.getBucket<String>(bucketName)
         bucket.set(value)
         println("bucket.get(): ${bucket.get()}")
     }
 
-    private fun objectGetSet(redissonClient: RedissonClient, bookPages: Int, bookChapter: Int, bookAuthor: String) {
-        printHelper("objectGetSet")
+    private fun `object`(redissonClient: RedissonClient, bookPages: Int, bookChapter: Int, bookAuthor: String) {
+        printHelper("object")
         val bucket: RBucket<Book> = redissonClient.getBucket("book")
         val book1 = Book(bookPages, bookChapter, bookAuthor)
         bucket.set(book1)
@@ -64,6 +128,16 @@ class App {
         println("getBook5 : $getBook5")
     }
 
+    private fun keys(redissonClient: RedissonClient, keyName1: String, keyName2: String) {
+        printHelper("keys")
+        val rKeys: RKeys = redissonClient.keys
+        redissonClient.getBucket<Int>(keyName1).set(1)
+        redissonClient.getBucket<Int>(keyName2).set(1)
+        val keys = rKeys.keys
+        println("default sorted (almost lexicographically) keys:")
+        keys.sorted().forEach { println(it) }
+    }
+
     private fun printHelper(content: String) {
         println("------------------------------")
         println("--- $content function output: ")
@@ -81,9 +155,11 @@ class App {
         // Pipelining
         // Scripting
         return if (redisson != null) {
-            bucketGetSet(redisson, "foo", "bar") // buckets
-            objectGetSet(redisson, 100, 10, "some author")
-            keysSetGet(redisson, "test1", "test2")
+            atomicLong(redisson, 3L)
+            atomicLongAsync(redisson, 3L)
+            bucket(redisson, "foo", "bar") // buckets
+            `object`(redisson, 100, 10, "some author")
+            keys(redisson, "test1", "test2")
             redisson.shutdown()
             true
         } else {
