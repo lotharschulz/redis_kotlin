5acaf833b207335f3efbc0928c72ca26c58fd11f
Lothar Schulz <lothar.sch@gmx.de>
2023-04-01 09:32:41 +0200

    chore: comments whats next

--- start-docker-run-code-stop-docker.sh
+++ start-docker-run-code-stop-docker.sh
@@ -5,6 +5,14 @@ IFS=$'\n\t'
 
 dockerContainerName=my-redis-stack
 
+  # fuer running container
+  # und wie hier fuer exited container einbauen
+  #     if [ "$(docker ps -aq -f status=exited -f name=<name>)" ]; then
+  #        # cleanup
+  #        docker rm <name>
+  #    fi
+
+
 if [ ! "$(docker ps -a -q -f name=$dockerContainerName)" ]; then
     # run your container
     docker run -d --name $dockerContainerName -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
