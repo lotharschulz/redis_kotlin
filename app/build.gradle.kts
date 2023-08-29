0b7fd45b3e62d26d65c5e057f2f45c401b7ef8ce
Lothar Schulz <lothar.sch@gmx.de>
2023-08-29 21:46:44 +0200

    chore fix #21 added https://mvnrepository.com/artifact/org.slf4j/slf4j-nop/2.0.7 to gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 dependencies {
     implementation("org.redisson:redisson:3.19.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    // https://github.com/lotharschulz/redis_kotlin/issues/21
+    implementation("org.slf4j:slf4j-nop:2.0.7")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
